52e5f24f6e605601134264c3389f11eff2e8816a
Snoolf001 <abdurahmonovjaloliddin2002@gmail.com>
2023-03-23 10:25:05 +0500

    Some changes

--- core/urls.py
+++ core/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from api.models import HomeView
+from api.views import HomeView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
