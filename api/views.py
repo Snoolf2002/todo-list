5437a7b94d08626c7cbe051ab18f2ec4f6f40001
Snoolf001 <abdurahmonovjaloliddin2002@gmail.com>
2023-03-23 10:19:55 +0500

    Created get request for HomeView

--- api/views.py
+++ api/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
-
+from django.views import View
+from django.http import HttpResponse, JsonResponse
+from django.contrib.auth.models import User
 # Create your views here.
+
+
+class HomeView(View):
+    def get(self, request):
+        users = User.objects.all()
+
+        data = {
+            'Users': []
+        }
+        for user in users:
+            data['Users'].append(
+                {
+                    'id': user.id,
+                    'username': user.username,
+                    'email': user.email
+                }
+            )
+        
+        return JsonResponse(data)
+    
+    def post(self, request):
+        return JsonResponse({"Message":"POST"})
